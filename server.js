29065b9a6eecc8090b800df09bfcec2c0e36e196
Maxim Rakhmanin <rakhmaxdev@gmail.com>
2021-01-02 19:30:11 +0300

    Fix port

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const { createServer } = require('http');
 
 const fileServer = new Server('./dist');
 
+const port = process.env.PORT || 8080;
+
 createServer((req, res) => {
   fileServer.serve(req, res, (e) => {
     if (e) {
       fileServer.serveFile('/index.html', 200, {}, req, res);
     }
   });
-}).listen(8080);
+}).listen(port);
