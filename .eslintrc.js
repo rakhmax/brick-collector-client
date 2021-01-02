2cf86cec977e42c161561bb9a9e2b4c506649547
Maxim Rakhmanin <rakhmaxdev@gmail.com>
2021-01-02 17:54:16 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'vue/valid-v-slot': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+};
